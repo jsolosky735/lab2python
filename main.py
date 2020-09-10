fc139f532dda969337c668e61a30be4fb8008194
jsolosky735 <jjs7331@psu.edu>
2020-09-10 18:51:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#Author: Jamys Solosky jjs7331@psu.edu
+#Collaborator: Balade Shafa bks5681@psu.edu
+#Collaborator: Patrick Podobinski pbp5221@psu.edu
+#Collaborator: Clare Robson cir5887@psu.edu
+#Section: 6
+#Breakout:13
+def getLetterGrade (grade):
+  if grade >= 93.0:
+    gradel = "A"
+  elif grade >= 90.0:
+    gradel = "A-"
+  elif grade >= 87.0:
+    gradel = "B+"
+  elif grade >= 83.0:
+    gradel = "B"
+  elif grade >= 80.0:
+    gradel = "B-"
+  elif grade >= 77.0:
+    gradel = "C+"
+  elif grade >= 70.0:
+    gradel = "C"
+  elif grade >= 60.0:
+    gradel = "D"
+  else:
+    gradel = "F"
+  return gradel
+
+def run():
+  grade = input("Enter your CMPSC 131 grade: ")
+  grade = float(grade)
+  print(getLetterGrade(grade))
+  print(f"Your letter grade for CMPSC 131 is {gradel}.")
+
+if __name__ == "__main__":
+  run() 
+
+
+
+  
